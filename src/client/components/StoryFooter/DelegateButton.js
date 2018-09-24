b375f20de9fc0feec657efa99852b5e3be4becce
East <eastmaels@gmail.com>
2018-09-24 16:09:53 +0900

    added ulogs as delegatee to delegate button; replaced custom SP with link to steembottracker's delegation page

--- src/client/components/StoryFooter/DelegateButton.js
+++ src/client/components/StoryFooter/DelegateButton.js
@@ -17,76 +17,71 @@ class DelegateButton extends React.Component {
   constructor(props) {
     super(props);
 
-    this.state = {
-      delegateSpMenuVisible: false,
-    };
-
     this.handleInputCustomSP = this.handleInputCustomSP.bind(this);
-    this.handleDelegateSpMenuClick = this.handleDelegateSpMenuClick.bind(this);
-    this.handleDelegateSpVisibleChange = this.handleDelegateSpVisibleChange.bind(this);
   }
 
   handleInputCustomSP = (value) => {
     this.setState({ customSP: value });
   }
 
-  handleDelegateSpMenuClick = (e) => {
-    if (e.key !== 'custom') {
-      this.setState({ delegateSpMenuVisible: false });
-    }
-  }
-
-  handleDelegateSpVisibleChange = (flag) => {
-    this.setState({ delegateSpMenuVisible: flag });
+  renderDelegateSpSubMenu(delegatee, delegationAmounts) {
+    let delegateSpSubMenu = delegationAmounts.map(
+      amount => {
+        const delegateQuery = {
+          delegatee: delegatee,
+          vesting_shares: `${amount} SP`,
+        };
+        return (
+          <Menu.Item
+            key={amount}
+            style={{fontSize: '14px'}}
+          >
+            <Link
+              target='_blank'
+              to={SteemConnect.sign('delegateVestingShares', delegateQuery)}
+            >
+              <Icon type='rocket' /> {amount} SP
+            </Link>
+          </Menu.Item>
+        );
+      }
+    );
+    delegateSpSubMenu = [
+      ...delegateSpSubMenu,
+      <Menu.Item key={'custom-' + delegatee}>
+        <Link
+          target='_blank'
+          to={'https://steembottracker.com/delegation.html?delegatee=' + delegatee}
+        >
+          <Icon type='rocket' /> Custom SP
+        </Link>
+      </Menu.Item>
+    ];
+    return (delegateSpSubMenu);
   }
 
   render() {
     const { post } = this.props;
+    const SubMenu = Menu.SubMenu;
+
     const delegateSpMenu = (
-      <Menu
-        onClick={this.handleDelegateSpMenuClick}
-        style={{ marginLeft: '50%' }}
-      >
-        {delegationAmounts.map(
-          amount => {
-            const delegateQuery = {
-              delegatee: post.author,
-              vesting_shares: `${amount} SP`,
-            };
-            return (
-              <Menu.Item
-                key={amount}
-                style={{fontSize: '14px'}}
-              >
-                <Link
-                  target='_blank'
-                  to={SteemConnect.sign('delegateVestingShares', delegateQuery)}
-                >
-                  <Icon type='rocket' /> {amount} SP
-                </Link>
-              </Menu.Item>
-            );
+      <Menu>
+        <SubMenu
+          key={ post.author + '-sub' }
+          title={
+            <span><Icon type='user' /><span>@{post.author}</span></span>
           }
-        )}
-        <Menu.Item key={'custom'}>
-          <Input.Group>
-            <InputNumber
-              type={'number'}
-              onChange={this.handleInputCustomSP}
-              style={{ width: '78%', fontSize: '14px' }}
-              min={0}
-            />
-            <Button
-              type='primary'
-              shape='circle'
-              icon='rocket'
-              target='_blank'
-              href={SteemConnect.sign('delegateVestingShares', {delegatee: post.author, vesting_shares: `${this.state.customSP} SP`,})}
-              style={{ margin: '0px 5px' }}
-              size={'small'}
-            />
-          </Input.Group>
-        </Menu.Item>
+        >
+          {this.renderDelegateSpSubMenu(post.author, delegationAmounts)}
+        </SubMenu>
+        <SubMenu
+          key='ulogs-sub'
+          title={
+            <span><Icon type='user' /><span>@ulogs</span></span>
+          }
+        >
+          {this.renderDelegateSpSubMenu('ulogs', delegationAmounts)}
+        </SubMenu>
       </Menu>
     );
 
@@ -94,8 +89,6 @@ class DelegateButton extends React.Component {
       <Dropdown
         overlay={delegateSpMenu}
         trigger={['click']}
-        onVisibleChange={this.handleDelegateSpVisibleChange}
-        visible={this.state.delegateSpMenuVisible}
         placement={'bottomRight'}
       >
         <Button
