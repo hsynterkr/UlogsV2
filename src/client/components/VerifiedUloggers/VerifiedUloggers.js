b67b6258a48aaf8e639335f7daa65234edfa7fc1
sleepy3482 <43634140+sleepy3482@users.noreply.github.com>
2018-09-30 04:58:01 +0000

    updated verified

--- src/client/components/VerifiedUloggers/VerifiedUloggers.js
+++ src/client/components/VerifiedUloggers/VerifiedUloggers.js
@@ -3,30 +3,42 @@ import {Row,Col} from 'antd';
 import FollowButton from '../../widgets/FollowButton.js';
 import DelegateButton from '../StoryFooter/DelegateButton.js';
 import './VerifiedUloggers.css';
-import ContactButton from '../StoryFooter/ContactButton.js';
 import Avatar from '../Avatar.js';
+import ContactButton from '../Sidebar/WalletSidebar.js'
 class VerifiedUloggers extends React.Component{
     constructor(props){
         super(props);
+        this.state = {
+            post:{
+                author:this.props.username
+            }
+        }
     }
     render(){
+    const topStyle = {
+      marginBottom:'5%'
+    };
         return(
             <div className='VerifiedUloggers'>
+                <Row style={topStyle} gutter={ {xs: 8, sm: 16, md: 24, lg: 32} }>
+                    <div className='topVerified' >
+                        <Col span={8}>
+                            <Avatar username={this.props.username} size={40}/>
+                        </Col>
+                        <Col span={8}>
+                            <h2>{'@'+this.props.username}</h2>
+                        </Col>
+                    </div>
+                </Row>
                 <Row gutter={ {xs: 8, sm: 16, md: 24, lg: 32} }>
-                    <Col span={2}>
-                        <Avatar username={this.props.username} size={40}/>
+                    <Col span={8}>
+                                  <FollowButton username={this.props.username}/>
                     </Col>
-                    <Col span={6}>
-                        <p>{'@'+this.props.username}</p>
+                    <Col span={8}>
+                        <DelegateButton post={this.state.post}/>
                     </Col>
-                    <Col span={5}>
-                        <FollowButton username={this.props.username}/>
-                    </Col>
-                    <Col span={5}>
-                        <DelegateButton/>
-                    </Col>
-                    <Col span={6}>
-                        <ContactButton url={this.props.contactURL}/>
+                    <Col span={8}>
+                        <ContactButton username={this.props.username} verified={true} />
                     </Col>
                 </Row>
             </div>
@@ -35,3 +47,5 @@ class VerifiedUloggers extends React.Component{
 }
 
 export default VerifiedUloggers;
+                                     
+
