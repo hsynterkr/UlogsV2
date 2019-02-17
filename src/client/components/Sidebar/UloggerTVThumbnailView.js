a29eeb6fa4df4684cf4f5dcaed3631d332933cb3
Shubham Vijay Vargiy <svijay1692@gmail.com>
2019-02-17 16:47:11 +0530

    UI Fixes

---
+++ src/client/components/Sidebar/UloggerTVThumbnailView.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import '../Avatar.less';
+import './User.less';
+
+const UloggerTVThumbnail = ({ video }) => (
+  <div className="User__links_overflow_x_auto">
+    <a href={`https://www.youtube.com/watch?v=${video.id.videoId}`}>
+      <div
+        className="Avatar"
+        style={{
+          backgroundImage: `url(${video.snippet.thumbnails.default.url})`,
+          minWidth: '34px',
+          width: '34px',
+          height: '34px',
+          margin: `0 10px`,
+        }}
+      />
+    </a>
+  </div>
+);
+
+UloggerTVThumbnail.propTypes = {
+  video: PropTypes.shape().isRequired,
+};
+
+export default UloggerTVThumbnail;
