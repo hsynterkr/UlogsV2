a18a68e6bb634f17835d7cb5b1b8c5479df1e56d
surpassinggoogle <comsilbronze4@gmail.com>
2019-02-24 21:17:56 +0800

    Revert "Revert "Partial Fix For #183 And #184""

--- src/client/app/Sidebar/RightSidebar.js
+++ src/client/app/Sidebar/RightSidebar.js
@@ -14,8 +14,6 @@ import { checkWitnessVote } from '../../helpers/voteHelpers';
 import { updateRecommendations } from '../../user/userActions';
 import InterestingUloggersWithAPI from '../../components/Sidebar/InterestingUloggersWithAPI';
 import UlogStories from '../../components/Sidebar/UlogStories';
-import ChatBar from '../../components/Sidebar/ChatBar';
-import UlogGamesExchanges from '../../components/Sidebar/UlogGamesExchanges';
 import SignUp from '../../components/Sidebar/SignUp';
 import WitnessVote from '../../components/Sidebar/WitnessVote';
 import PostRecommendation from '../../components/Sidebar/PostRecommendation';
@@ -23,6 +21,8 @@ import Loading from '../../components/Icon/Loading';
 import UserActivitySearch from '../../activity/UserActivitySearch';
 import WalletSidebar from '../../components/Sidebar/WalletSidebar';
 import FeedSidebar from '../../components/Sidebar/FeedSidebar';
+import ChatBar from '../../components/Sidebar/ChatBar';
+import UlogGamesExchanges from '../../components/Sidebar/UlogGamesExchanges';
 
 @withRouter
 @connect(
@@ -47,6 +47,7 @@ export default class RightSidebar extends React.Component {
     updateRecommendations: PropTypes.func,
     followingList: PropTypes.arrayOf(PropTypes.string).isRequired,
     isFetchingFollowingList: PropTypes.bool.isRequired,
+    match: PropTypes.shape().isRequired,
   };
 
   static defaultProps = {
@@ -64,6 +65,7 @@ export default class RightSidebar extends React.Component {
       isAuthFetching,
       followingList,
       isFetchingFollowingList,
+      match,
     } = this.props;
 
     if (isAuthFetching) {
@@ -79,10 +81,22 @@ export default class RightSidebar extends React.Component {
       }
     }
     const isWitnessVoted = checkVote();
-
+    const { category } = match.params;
+    const displayUlogCaption =
+      category &&
+      category.match(
+        /^(ulog-quotes|ulog-howto|ulog-diy|ulog-surpassinggoogle|teardrops|untalented|ulog-ned|ulography|ulog-gratefulvibes|ulog-resolutions|ulog-memes|ulog-blocktrades|ulog-showerthoughts|ulog-snookmademedoit|ulog-utopian|ulog-thejohalfiles|ulogifs|ulog-surfyogi|ulog-bobbylee|ulog-stellabelle|ulog-sweetsssj|ulog-dimimp|ulog-teamsteem|ulog-kusknee|ulog-papapepper|ulog-steemjet)$/,
+      );
+    console.log('displayUlogCaption', displayUlogCaption);
     return (
       <div>
         {!authenticated && <SignUp />}
+        {displayUlogCaption &&
+          !authenticated && <UlogGamesExchanges isFetchingFollowingList={false} />}
+        {displayUlogCaption &&
+          !authenticated && (
+            <ChatBar isFetchingFollowingList={false} authenticated={authenticated} />
+          )}
         <Switch>
           <Route path="/activity" component={UserActivitySearch} />
           <Route path="/@:name/activity" component={UserActivitySearch} />
@@ -109,17 +123,7 @@ export default class RightSidebar extends React.Component {
               <div>
                 {authenticated && !showPostRecommendation ? (
                   <div>
-                    <UlogGamesExchanges
-                      authenticatedUser={authenticatedUser}
-                      followingList={followingList}
-                      isFetchingFollowingList={isFetchingFollowingList}
-                    />
                     <UlogStories
-                      authenticatedUser={authenticatedUser}
-                      followingList={followingList}
-                      isFetchingFollowingList={isFetchingFollowingList}
-                    />
-                    <ChatBar
                       authenticatedUser={authenticatedUser}
                       followingList={followingList}
                       isFetchingFollowingList={isFetchingFollowingList}
@@ -133,11 +137,6 @@ export default class RightSidebar extends React.Component {
                 ) : (
                   <div>
                     <UlogStories
-                      authenticatedUser={authenticatedUser}
-                      followingList={followingList}
-                      isFetchingFollowingList={isFetchingFollowingList}
-                    />
-                    <UlogGamesExchanges
                       authenticatedUser={authenticatedUser}
                       followingList={followingList}
                       isFetchingFollowingList={isFetchingFollowingList}
--- src/client/feed/Page.js
+++ src/client/feed/Page.js
@@ -7,7 +7,6 @@ import YoutubePlayer from 'react-player/lib/players/YouTube';
 import { getFeedContent } from './feedActions';
 import { getIsLoaded, getIsAuthenticated } from '../reducers';
 import SubFeed from './SubFeed';
-import UlogCaption from './UlogCaption';
 import HeroBannerContainer from './HeroBannerContainer';
 import LeftSidebar from '../app/Sidebar/LeftSidebar';
 import RightSidebar from '../app/Sidebar/RightSidebar';
@@ -18,6 +17,7 @@ import ScrollToTop from '../components/Utils/ScrollToTop';
 import ScrollToTopOnMount from '../components/Utils/ScrollToTopOnMount';
 import QuickPostEditor from '../components/QuickPostEditor/QuickPostEditor';
 import MainMenu from '../components/MainMenu';
+import UlogsBanner from '../components/UlogsBanner';
 import './Feed.less';
 
 @connect(state => ({
@@ -55,13 +55,12 @@ class Page extends React.Component {
 
     const shouldDisplaySelector = location.pathname !== '/' || (!authenticated && loaded);
     const displayTopicSelector = location.pathname === '/trending';
+    const robots = location.pathname === '/' ? 'index,follow' : 'noindex,follow';
     const displayUlogCaption =
       category &&
       category.match(
         /^(ulog-quotes|ulog-howto|ulog-diy|ulog-surpassinggoogle|teardrops|untalented|ulog-ned|ulography|ulog-gratefulvibes|ulog-resolutions|ulog-memes|ulog-blocktrades|ulog-showerthoughts|ulog-snookmademedoit|ulog-utopian|ulog-thejohalfiles|ulogifs|ulog-surfyogi|ulog-bobbylee|ulog-stellabelle|ulog-sweetsssj|ulog-dimimp|ulog-teamsteem|ulog-kusknee|ulog-papapepper|ulog-steemjet)$/,
       );
-    const robots = location.pathname === '/' ? 'index,follow' : 'noindex,follow';
-
     return (
       <div>
         <Helmet>
@@ -79,54 +78,61 @@ class Page extends React.Component {
               </a>
             </div>
             <YoutubePlayer
-              className='youtube-player'
+              className="youtube-player"
               url="https://youtu.be/5tq_rCZURUg"
-              width= '100%'
-              controls />
+              width="100%"
+              controls
+            />
             <div>
               <a href="ulog/@uloggers/uloggers-today-s-certified-and-verified-ulogger-true-celebrity-is-enjieneer-join-in-as-her-true-fans-we-will-fix-many-worries">
                 <img width={'100%'} height={'100%'} alt="900x500" src="/images/slide2.jpg" />
               </a>
             </div>
             <YoutubePlayer
-              className='youtube-player'
+              className="youtube-player"
               url="https://youtu.be/pKoW5HJ1l84"
-              width= '100%'
-              controls />
+              width="100%"
+              controls
+            />
             <YoutubePlayer
-              className='youtube-player'
+              className="youtube-player"
               url="https://youtu.be/K8G97hEls_U"
-              width= '100%'
-              controls />
+              width="100%"
+              controls
+            />
             <div>
               <a href="ulog/@uloggers/uloggers-today-s-certified-and-verified-ulogger-true-celebrity-is-kneelyrac-join-in-as-her-true-fans-we-will-fix-many-worries">
                 <img width={'100%'} height={'100%'} alt="900x500" src="/images/slide3.jpg" />
               </a>
             </div>
             <YoutubePlayer
-              className='youtube-player'
+              className="youtube-player"
               url="https://youtu.be/kKZ1CixLG2s"
-              width= '100%'
-              controls />
+              width="100%"
+              controls
+            />
             <YoutubePlayer
-              className='youtube-player'
+              className="youtube-player"
               url="https://youtu.be/UqNsKU4lnLo"
-              width= '100%'
-              controls />
+              width="100%"
+              controls
+            />
             <div>
               <a href="ulog/@uloggers/uloggers-today-s-certified-and-verified-ulogger-true-celebrity-is-sn0white-join-in-as-her-true-fans-we-will-fix-many-worries">
                 <img width={'100%'} height={'100%'} alt="900x500" src="/images/slide4.jpg" />
               </a>
             </div>
             <YoutubePlayer
-              className='youtube-player'
+              className="youtube-player"
               url="https://youtu.be/MAPKUato1K8"
-              width= '100%'
-              controls />
+              width="100%"
+              controls
+            />
           </Carousel>
         ) : (
           <HeroBannerContainer />
         )}
+        {displayUlogCaption && <UlogsBanner category={category} />}
         <MainMenu />
         <div className="shifted">
           <div className="feed-layout container">
@@ -141,7 +147,6 @@ class Page extends React.Component {
               </div>
             </Affix>
             <div className="center">
-              {displayUlogCaption && <UlogCaption category={category} />}
               {displayTopicSelector && <TrendingTagsMenu />}
               {shouldDisplaySelector && (
                 <TopicSelector
