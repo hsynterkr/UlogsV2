896a3d0126768370236d89ef6d82b8fb9122e5b1
East <eastmaels@gmail.com>
2019-03-19 06:10:43 +0900

    grow section constants

---
+++ src/client/helpers/growSections.js
@@ -0,0 +1,19 @@
+export const aboutGrow = `
+On Ulogs.org, it is **"true celebrity-hood for everyone, once and for all"**. We measure our success in terms of **'real human growth'**. We want to create **more value for 'humans'** in a world were cryptokitties, superman, millionaire-cats, human-clones etc is beginning to garner more value than 'humans'.
+
+Thus, we will incessantly seek innovative ways of inciting each ulogger to **'mine the human' into its awesomest version**. In the future, we will seek to celebrate a world filled with **'true celebrities'**, using the [TearDrops SMT](https://ulogs.org/teardrops/@surpassinggoogle/the-teardrops-smt-is-still-in-its-imaginary-state-but-it-is-time-to-start-gathering-read-this-and-consider-joining-our-telegram), 'a breakthrough token & emblem of human' that will reward 'proof of tears', for en-route attaining the awesomest version of human, there will be many tears, happy, sad or un-fell.
+
+We seek to remove 'all barriers to entry' from 'everything good', so that every(any)one can shine. The world needs more shine! @surpassinggoogle
+`;
+
+export const submitAVideo = `
+Across the ulogs.org ecosystem, we have 3 TV(s) i.e [UloggersTV](https://www.youtube.com/channel/UCzI3Rjamg7zSe_o0BwSeIQQ), [UntalentedTV](https://www.youtube.com/channel/UCq6ylwOaSG9VDYwZkMr3jMw) and [TeardropsTV](https://www.youtube.com/channel/UCnIJlzMZzfrcE0bXabLpXKw). We seek to use the media to impact the world and you can help matters. **We welcome your contributions**. We will feature your contribution on a corresponding TV. **In the future, your contributions may feature in our Uloggers Game App**.
+
+While we only feature *'certified uloggers'* on our TV(s), if you are not yet a [*'certified ulogger'*](https://ulogs.org/discover) and you submit a video, **we will also look to feature your video** and once we do so, **you will become a 'certified ulogger'**.
+`
+
+export const writeToUlogs = `
+The ulogs.org ecosystem also has an [un(dis)talented paradigm](https://ulogs.org/steemit/@surpassinggoogle/steemit-s-untalented-is-in-beta-participate-freely-because-every-participant-in-this-contest-will-win-something-no-losers). Thus, when you visit this space, **relegate reservations** and write to us freely. Whatever your dream is, we believe that in a **world of 'uloggers' (true celebrities) every noble dream is build-able**. 
+
+Tell us what you want and let's discuss. Don't undervalue the power of 'conversation', for at any moment, it can pop out something. Tell us of your forgotten dreams, let's resuscitate it. Do you need inspiration, assistance, mentoring, direction etc? Then, **write to us!** We are building an ecosystem of 'true celebrities', where everyone is your 'true fan'. At the very least, *'you are the celebrity; we are your fan'.*
+`
