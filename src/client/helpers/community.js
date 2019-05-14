7bfab5bc18dbd689448cd993696ba110210cb64b
East <eastmaels@gmail.com>
2019-05-15 06:52:33 +0900

    add create community page

---
+++ src/client/helpers/community.js
@@ -0,0 +1,17 @@
+export const aboutCommunities = `
+"Communities" is a core aspect of the ulogs.org ecosystem. A ulog-community is a community born around the art of ulogging. &nbsp;  
+&nbsp;  
+A ulog-subtag however, is a simple fusion between #ulog and a hashtag. Ulog-subtags simplifies the art of ulogging while maintaining its essence, by allowing users to ulog about niche-based topics. It also further incites users to be inventive with content-creation, unites existing STEEM communities by drawing "true fans" towards these communities and enhances content creation/curation. &nbsp;  
+&nbsp;  
+Each ulog-subtag has the potential of emanating a ulog-community. You can take a step towards creating a ulog-community, if you are a "certified ulogger". &nbsp;  
+&nbsp;
+
+Visit https://ulogs.org/discover to see if you are "certified". To become a "certified ulogger", contact us.
+
+&nbsp;
+&nbsp;
+
+Read more about ulog-subtags & ulog-communities here!
+`;
+
+export const createCommunity = `Ulog-Community Name (Whatever name you chose will be prefixed by "#ulog-")`;
